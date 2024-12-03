2b4003eed874bc083bc66b3e63817e1c332cf62c
roxxaaa <163797452+roxxaaa@users.noreply.github.com>
2024-12-03 23:47:19 +0800

    Update obj4.py

--- obj4.py
+++ obj4.py
@@ -104,7 +104,7 @@ def objective4(df, selected_municipalities, start_date, end_date):
     # Add an explanation about strong correlations summary
     st.markdown("""
     **Strong Correlations Summary**:
-    -         st.write("Strong correlations (above 0.7 or below -0.7) indicate significant relationships between factors influencing rice production."). 
+    ("Strong correlations (above 0.7 or below -0.7) indicate significant relationships between factors influencing rice production."). 
     """)
 
     # Display the correlation summary
